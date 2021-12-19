34c8b96ea5fafe2671e5997ab32d38630b3ead99
Imtiaj-NEUB <ahmedimtiaj91@gmail.com>
2021-12-20 01:25:55 +0600

    added missing file which was ignored by git

---
+++ storage/framework/views/b63110f0a77e6fc3803386017904de34d2a769bb.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Blog</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"/>
+    </head>
+    <body>
+        <div id="app"></div>  
+        <script src="js/app.js"></script>
+    </body>
+</html>
+<?php /**PATH G:\xampp\htdocs\work\blog\blog\resources\views/welcome.blade.php ENDPATH**/ ?>
